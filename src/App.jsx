9ac77ca20dde89a1052de524a727c372cba6dc1c
Yegor Shabanov <yegorshabanov@Yegors-MBP-2.lan>
2023-03-19 10:53:20 -0700

    update to fetch url

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import useMongoDB from './component/data/useMongoDB';
 
 function App() {
 
-  const { data, loading, error } = useFetch('https://yegorka78.github.io/gscars/data/db.json')
+  const { data, loading, error } = useFetch('https://greg-codez.github.io/gscars/data/db.json')
 //  useMongoDB()
 
   if (loading) return <h1> LOADING ... </h1>
