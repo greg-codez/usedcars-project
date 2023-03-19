1e546219f50160784ba1ead8c1689b95250d9cf9
Yegor Shabanov <yegorshabanov@Yegors-MBP-2.lan>
2023-03-19 16:09:36 -0700

    mode clean up to modal

--- src/component/CarsList/component/card/ProductCard.jsx
+++ src/component/CarsList/component/card/ProductCard.jsx
@@ -21,22 +21,24 @@ const ProductCard = ({ data }) => {
           <button className='learnMore' data-modal-target={data.id} value={data.id} onClick={() => modalCarContent(data)}>Learn more</button>
         </div>
       {openModal && carContent &&
-            <div>
-                <div className='modal' id={`modal-${carContent.id}`}>
-                    <div className='modal-header'>
-                        <div className='title'>Vehicle Information</div>
-                        <button data-close-button className='close-button' onClick={() => setOpenModal(false)}>&times;</button>
-                    </div>
-                    <div className='modal-body'>
-                        <div className='car-name'>{carContent.year}&nbsp;{carContent.make}&nbsp;{carContent.model}</div>
-                        <div className='car-text'>{carContent.submodel}</div>
-                        
-                        <div className='car-text'>{carContent.engine}&nbsp;{carContent.transmission}</div>
-                        <div style={{ background: carContent.colorHex, width: `10px`, height: `10px` }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-                        <div className='car-text'>{carContent.color}-{carContent.colorHex}</div>
-                    </div>
+
+          <div className="modal-container" id={`modal-${carContent.id}`}>
+              <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <div className="picture">
+                <img src={data.image} alt={data.name} />
                 </div>
-                <div id='overlay'></div>
+                <div className="text-wrap">
+                  <h2>{carContent.year}&nbsp;{carContent.make}&nbsp;{carContent.model}</h2>
+                  <p>{carContent.engine}&nbsp;{carContent.transmission}</p>
+                  <p>{carContent.engine}&nbsp;{carContent.transmission}</p>
+                  
+                </div>
+                <div className="close-btn" onClick={() => setOpenModal(false)}>
+                  &times;
+                </div>
+              </div>
+
+              <div id='overlay'></div>
             </div>
             }
       </div>
